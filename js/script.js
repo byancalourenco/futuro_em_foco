6c929858cb8a7142a133ceaacd3425cedb1be681
AnaClaraRivas <anaclararivasbronzeri@gmail.com>
2025-09-09 19:48:57 -0300

    menu cell

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+const menuToggle = document.getElementById('menu-toggle');
+const menuDesktop = document.querySelector('.menu-desktop');
+
+menuToggle.addEventListener('click', () => {
+    menuDesktop.classList.toggle('active');
+});
